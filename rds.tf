70222c264a31bcf67e4a921a800d196007905556
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-07-15 18:01:02 +0200

    general changes, switching from mssql to postgresql for the project

--- rds.tf
+++ rds.tf
@@ -2,6 +2,7 @@
 # RDS PostgreSql instance, original registry docs says you should add "db_name", but keep in mind that mssql doesnt allow you to do that, atleast this was the issues i stumbled into several times and wondered why.
 
 resource "aws_db_instance" "postgresql" {
+  identifier            = "my-postgresql"
   allocated_storage      = 20
   engine                 = "postgres"
   engine_version         = "17.4"          # supported version in your eu-central-1
@@ -9,7 +10,6 @@ resource "aws_db_instance" "postgresql" {
   username               = var.username
   password               = var.password
   db_name                = "mydb"          # PostgreSQL supports DB name at create
-  parameter_group_name   = "default.postgres15"  # Check your region
   skip_final_snapshot    = true
 
   db_subnet_group_name   = aws_db_subnet_group.db-subnet-group.name
