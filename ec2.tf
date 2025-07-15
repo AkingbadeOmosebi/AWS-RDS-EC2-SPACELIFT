b2a1fee63be63e443b231f6b0095ec390319b3cd
AkingbadeOmosebi <akingbadeomosebi@gmail.com>
2025-07-15 14:49:34 +0200

    gave a tag to my ec2

--- ec2.tf
+++ ec2.tf
@@ -4,4 +4,10 @@ resource "aws_instance" "ec2-mssql" {
   subnet_id              = aws_subnet.db-subnet01.id
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   key_name               = var.existing_key_pair_name
+
+   tags = {
+    Name = "ec2-mssql-instance"
+    Project = "db"
+    
+  }
 }
